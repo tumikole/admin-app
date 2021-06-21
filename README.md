20ddc1ee187d400019feb805d2d9b7961769f972
tumikole <ramollosamuel2@gmail.com>
2021-06-21 16:00:47 +0200

    Changed from pure function to class constructor functions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # admin-app
+
+On my admin app, i started by structuring the front page by putting the form which has the first name, last name, Identity number, work position and salary.
+
+I added another HTML page where theres a table, all the information from the form are stored in the table on another HTML page.
+I added a nav-bar which has a header that has the title of the app, and also a button (Stuff Member) that leads to the table page where the information from the form are stored.
+
+I also added javaScript file that execute functions for storing information to the table. Another function that stores the information to the LocalStorage, converting the objects to the Strings and also getting the information from the localStorage to display on table.
--- main.js
+++ main.js
@@ -1,40 +1,60 @@
-// let employeeList = []
-const register = event => {
-    event.preventDefault()
-    let formData = {
-        validationCustom01: document.getElementById("validationCustom01").value,
-        validationCustom02: document.getElementById("validationCustom02").value,
-        validationCustom03: document.getElementById("validationCustom03").value,
-        validationCustom04: document.getElementById("validationCustom04").value,
-        validationCustom05: document.getElementById("validationCustom05").value
+window.onload = function() {
+    myForm = document.getElementById("myForm");
+
+    addEventListener('submit', function(event) {
+        event.preventDefault();
+        firstName = document.getElementById("fname").value,
+            lastName = document.getElementById("lname").value,
+            idNumber = document.getElementById("idNumb").value,
+            position = document.getElementById("position").value,
+            salary = document.getElementById("salary").value
+        output = document.getElementById("output");
+        console.log(output)
+            // console.log(firstName, lastName, idNumber, position, salary)
+        var employeeList = new EmployeeList(firstName, lastName, idNumber, position, salary)
+        console.log(employeeList)
+        UI.displayData(employeeList);
+        UI.clearFields();
+
+
+
+    });
+    class EmployeeList {
+        constructor(firstName, lastName, idNumber, position, salary) {
+            this.firstName = firstName
+            this.lastName = lastName
+            this.idNumber = idNumber
+            this.position = position
+            this.salary = salary
+        }
     }
-    displayData()
-    localStorage.setItem('formData', JSON.stringify(employeeList));
-    event.preventDefault();
+
+    class UI {
+        static clearFields() {
+            document.getElementById("fname").value = ""
+            document.getElementById("lname").value = ""
+            document.getElementById("idNumb").value = ""
+            document.getElementById("position").value = ""
+            document.getElementById("salary").value = ""
+        }
+        static displayData(employeeList) {
+
+            UI.populatedRow(employeeList)
+        }
+        static populatedRow(employeeList) {
+            output.innerHTML = `<tr>
+                                                            <td>${employeeList.firstName}</td>
+                                                             <td>${employeeList.lastName}</td>
+                                                             <td>${employeeList.idNumber}</td>
+                                                              <td>${employeeList.position}
+                                                              <button type="button" class="btn btn-secondary">Edit<i class="far fa-trash-alt"></i></button>
+                                                              <button type="button" class="btn btn-warning">ave</button></td>
+                                                                 <td>${employeeList.salary}
+                                                                 <button type="button" class="btn btn-secondary">Edit<i class="far fa-trash-alt"></i></button>
+                                                                 <button type="button" class="btn btn-warning">ave</button></td>
+                                                                 <td><button type="button" class="btn btn-danger">x</button></td>
+                                                         </tr>`
+        }
+    }
 }
-
-
-
-const displayData = () => {
-    window.addEventListener('load', displayData)
-
-    console.log(JSON.parse(localStorage.getItem('formData')))
-
-    let { validationCustom01, validationCustom02, validationCustom03, validationCustom04, validationCustom05 } = JSON.parse(localStorage.getItem('formData'));
-    var output = document.querySelector('#output');
-    output.innerHTML += `<tr>
-
-            <td>${data[i].validationCustom01} </td>
-            <td>${data[i].validationCustom02}</td>
-            <td>${data[i].validationCustom03}</td>
-            <td>${data[i].validationCustom04}
-            <button id="table_btn" type="button" class="btn btn-warning">Edit</button>
-            <button id="table_btn" type="button" class="btn btn-success">Save</button></td>
-            <td>${data[i].validationCustom05}
-            <button id="table_btn" type="button" class="btn btn-warning">Edit</button>
-            <button id="table_btn" type="button" class="btn btn-success">Save</button></td>
-
-
-                  </tr>`
-
-}
+UI.displayData()
