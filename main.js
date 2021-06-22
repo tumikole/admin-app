4980dc04c6080c186c8ffaf8ea8e76080c7a3e50
tumikole <ramollosamuel2@gmail.com>
2021-06-22 16:01:41 +0200

    adding edit button

--- main.js
+++ main.js
@@ -1,60 +1,95 @@
 window.onload = function() {
-    myForm = document.getElementById("myForm");
+        myForm = document.getElementById("myForm");
 
-    addEventListener('submit', function(event) {
-        event.preventDefault();
-        firstName = document.getElementById("fname").value,
-            lastName = document.getElementById("lname").value,
-            idNumber = document.getElementById("idNumb").value,
-            position = document.getElementById("position").value,
-            salary = document.getElementById("salary").value
-        output = document.getElementById("output");
-        console.log(output)
-            // console.log(firstName, lastName, idNumber, position, salary)
-        var employeeList = new EmployeeList(firstName, lastName, idNumber, position, salary)
-        console.log(employeeList)
-        UI.displayData(employeeList);
-        UI.clearFields();
+        addEventListener('submit', function(event) {
+            event.preventDefault();
+            firstName = document.getElementById("fname").value,
+                lastName = document.getElementById("lname").value,
+                idNumber = document.getElementById("idNumb").value,
+                position = document.getElementById("position").value,
+                salary = document.getElementById("salary").value
+            output = document.getElementById("output");
+            // console.log(output)
+                // console.log(firstName, lastName, idNumber, position, salary)
+            var employeeList = new EmployeeList(firstName, lastName, idNumber, position, salary)
+                // localStorage.setItem(JSON.stringify(List, EmployeeList));
+            // console.log(employeeList)
+            UI.displayData(employeeList);
+            Store.setStored(employeeList);
+            Store.getStored(employeeList)
+            UI.clearFields();
 
 
 
-    });
-    class EmployeeList {
-        constructor(firstName, lastName, idNumber, position, salary) {
-            this.firstName = firstName
-            this.lastName = lastName
-            this.idNumber = idNumber
-            this.position = position
-            this.salary = salary
-        }
-    }
+        });
+       
 
-    class UI {
-        static clearFields() {
-            document.getElementById("fname").value = ""
-            document.getElementById("lname").value = ""
-            document.getElementById("idNumb").value = ""
-            document.getElementById("position").value = ""
-            document.getElementById("salary").value = ""
-        }
-        static displayData(employeeList) {
+        output.addEventListener('click', function(event) {
 
-            UI.populatedRow(employeeList)
-        }
-        static populatedRow(employeeList) {
-            output.innerHTML = `<tr>
+                    UI.removeRow(event.target)
+
+        })
+                    class EmployeeList {
+                        constructor(firstName, lastName, idNumber, position, salary) {
+                            this.firstName = firstName
+                            this.lastName = lastName
+                            this.idNumber = idNumber
+                            this.position = position
+                            this.salary = salary
+                        }
+                    }
+
+                    class UI {
+                        static clearFields() {
+                            document.getElementById("fname").value = ""
+                            document.getElementById("lname").value = ""
+                            document.getElementById("idNumb").value = ""
+                            document.getElementById("position").value = ""
+                            document.getElementById("salary").value = ""
+                        }
+                        static displayData(employeeList) {
+
+                            UI.populatedRow(employeeList)
+                        }
+
+                        static populatedRow(employeeList) {
+                            output.innerHTML = `<tr>
                                                             <td>${employeeList.firstName}</td>
                                                              <td>${employeeList.lastName}</td>
                                                              <td>${employeeList.idNumber}</td>
                                                               <td>${employeeList.position}
-                                                              <button type="button" class="btn btn-secondary">Edit<i class="far fa-trash-alt"></i></button>
-                                                              <button type="button" class="btn btn-warning">ave</button></td>
+                                                              <button type="button" class="btn btn-secondary">Edit</button>
+                                                              <button type="button" class="btn btn-warning">Save</button></td>
                                                                  <td>${employeeList.salary}
-                                                                 <button type="button" class="btn btn-secondary">Edit<i class="far fa-trash-alt"></i></button>
-                                                                 <button type="button" class="btn btn-warning">ave</button></td>
-                                                                 <td><button type="button" class="btn btn-danger">x</button></td>
+                                                                 <button type="button" class="btn btn-secondary">Edit</button>
+                                                                 <button type="button" class="btn btn-warning">Save</button></td>
+                                                                 <td><button type="button" class="btn btn-danger removeIt">x</button></td>
                                                          </tr>`
-        }
-    }
-}
-UI.displayData()
+                        }
+
+                        static removeRow(element) {
+                            if (event.target.classList.contains('removeIt')) {
+                                element.parentElement.parentElement.remove();
+                            }
+                        }
+                    }
+                    class Store {
+                        static getStored() {
+                            let employeeList = ""
+
+                            if(localStorage.getItem('employeeList') == null){
+                                 return employeeList = []
+                            } else {
+                                employeeList = JSON.parse(localStorage.getItem('EmployeeList'))
+                            }
+                             return employeeList
+                        }
+                        static setStored(obj){
+                        
+                        let employeeListFromLocal = Store.getStored()
+                            employeeListFromLocal.splice(obj)
+                            console(obj)
+                            localStorage.setItem('employeeList', JSON.stringify(employeeListFromLocal))
+                        }
+                    }
+                }
