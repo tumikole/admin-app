3f280fd3a6b6a481858b7d4f832eee353cc26577
tumikole <ramollosamuel2@gmail.com>
2021-06-25 07:45:14 +0200

    adding js file

--- main.js
+++ main.js
@@ -1,95 +1,158 @@
-window.onload = function() {
-        myForm = document.getElementById("myForm");
+function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("mySearch");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }       
+        }
+      }
 
-        addEventListener('submit', function(event) {
-            event.preventDefault();
-            firstName = document.getElementById("fname").value,
-                lastName = document.getElementById("lname").value,
-                idNumber = document.getElementById("idNumb").value,
-                position = document.getElementById("position").value,
-                salary = document.getElementById("salary").value
-            output = document.getElementById("output");
-            // console.log(output)
-                // console.log(firstName, lastName, idNumber, position, salary)
-            var employeeList = new EmployeeList(firstName, lastName, idNumber, position, salary)
-                // localStorage.setItem(JSON.stringify(List, EmployeeList));
-            // console.log(employeeList)
-            UI.displayData(employeeList);
-            Store.setStored(employeeList);
-            Store.getStored(employeeList)
-            UI.clearFields();
+class Employee {
+        constructor(firstName, lastName, idNumber, position, salary) {
+                this.firstName = firstName
+                this.lastName = lastName
+                this.idNumber = idNumber
+                this.position = position
+                this.salary = salary
+        }
+}
 
+class UI {
+        static displayEmployees() {
+                const employees = Store.getEmployees();
+                employees.forEach((employee) => UI.addEmployeeToList(employee));
 
+        }
 
-        });
-       
+        static addEmployeeToList(employee) {
+                const list = document.querySelector('#output');
 
-        output.addEventListener('click', function(event) {
+                const row = document.createElement('tr');
 
-                    UI.removeRow(event.target)
+                row.innerHTML = `
+                <tr>
+                                                        <td>${employee.firstName}</td>
+                                                         <td>${employee.lastName}</td>
+                                                         <td>${employee.idNumber}</td>
+                                                          <td>${employee.position}
+                                                          <button type="button" class="btn editIt">Edit</button>
+                                                             <button type="button" class="btn saveIt">Save</button></td>
+                                                          </td>
+                                                             <td>${employee.salary}
+                                                             <button type="button" class="btn editIt">Edit</button>
+                                                             <button type="button" class="btn saveIt">Save</button></td>
+                                                             <td><button type="button" class="btn btn-danger delete">x</button></td>
+                                                     </tr>
+                                                     `;
+                list.appendChild(row);
+        }
 
-        })
-                    class EmployeeList {
-                        constructor(firstName, lastName, idNumber, position, salary) {
-                            this.firstName = firstName
-                            this.lastName = lastName
-                            this.idNumber = idNumber
-                            this.position = position
-                            this.salary = salary
-                        }
-                    }
-
-                    class UI {
-                        static clearFields() {
-                            document.getElementById("fname").value = ""
-                            document.getElementById("lname").value = ""
-                            document.getElementById("idNumb").value = ""
-                            document.getElementById("position").value = ""
-                            document.getElementById("salary").value = ""
-                        }
-                        static displayData(employeeList) {
-
-                            UI.populatedRow(employeeList)
-                        }
-
-                        static populatedRow(employeeList) {
-                            output.innerHTML = `<tr>
-                                                            <td>${employeeList.firstName}</td>
-                                                             <td>${employeeList.lastName}</td>
-                                                             <td>${employeeList.idNumber}</td>
-                                                              <td>${employeeList.position}
-                                                              <button type="button" class="btn btn-secondary">Edit</button>
-                                                              <button type="button" class="btn btn-warning">Save</button></td>
-                                                                 <td>${employeeList.salary}
-                                                                 <button type="button" class="btn btn-secondary">Edit</button>
-                                                                 <button type="button" class="btn btn-warning">Save</button></td>
-                                                                 <td><button type="button" class="btn btn-danger removeIt">x</button></td>
-                                                         </tr>`
-                        }
-
-                        static removeRow(element) {
-                            if (event.target.classList.contains('removeIt')) {
-                                element.parentElement.parentElement.remove();
-                            }
-                        }
-                    }
-                    class Store {
-                        static getStored() {
-                            let employeeList = ""
-
-                            if(localStorage.getItem('employeeList') == null){
-                                 return employeeList = []
-                            } else {
-                                employeeList = JSON.parse(localStorage.getItem('EmployeeList'))
-                            }
-                             return employeeList
-                        }
-                        static setStored(obj){
-                        
-                        let employeeListFromLocal = Store.getStored()
-                            employeeListFromLocal.splice(obj)
-                            console(obj)
-                            localStorage.setItem('employeeList', JSON.stringify(employeeListFromLocal))
-                        }
-                    }
+        static deleteEmployee(el) {
+                if (el.classList.contains('delete')) {
+                        el.parentElement.parentElement.remove();
                 }
+        }
+
+        // static showAlert(message, className){
+        // const div = document.createElement('div');
+        // div.className = `alert alert-${className}`;
+        // div.appendChild(document.createTextNode(message));
+        // const container = document.querySelector('.container');
+        // const form = document.querySelector("#myForm");
+        // container.insertBefore(div, form);
+        // }
+
+
+        static clearFields() {
+                document.querySelector('#fname').value = '';
+                document.querySelector('#lname').value = '';
+                document.querySelector('#idNumb').value = '';
+                document.querySelector('#position').value = '';
+                document.querySelector('#salary').value = '';
+        }
+}
+
+class Store {
+
+        static getEmployees() {
+
+                let employees;
+                if (localStorage.getItem('employee') === null) {
+
+                        employees = [];
+                } else {
+                        employees = JSON.parse(localStorage.getItem('employees'));
+                        return employees;
+                }
+        }
+
+        static addEmployee(employee) {
+                const employees = Store.getEmployees();
+                console.log(employee)
+
+                localStorage.setItem('employee', JSON.stringify(employees));
+                employees.push(employees);
+
+                localStorage.setItem('employees', JSON.stringify(employees));
+        }
+
+        static removeEmployee(idNumber) {
+                const employees = Store.getEmployees();
+
+                employees.forEach((employee, index) => {
+                        if (employee.idNumber === idNumber) {
+                                employees.splice(index, 1);
+                        }
+                });
+
+                localStorage.setItem('employees', JSON.stringify(employees));
+        }
+}
+
+document.addEventListener('DOMContentLoaded', UI.displayEmployees);
+
+document.querySelector('#myForm').addEventListener('submit', (event) => {
+
+        event.preventDefault();
+
+
+        const firstName = document.querySelector("#fname").value;
+        const lastName = document.querySelector("#lname").value;
+        const idNumber = document.querySelector("#idNumb").value;
+        const position = document.querySelector("#position").value;
+        const salary = document.querySelector("#salary").value;
+
+
+        if (firstName === '' || lastName === '' || idNumber === '' || position === '' || salary === '') {
+                alert('Please fill all fields', 'danger');
+        } else {
+
+                const employee = new Employee(firstName, lastName, idNumber, position, salary)
+
+                UI.addEmployeeToList(employee);
+
+                Store.addEmployee(employee)
+
+                // UI.showAlert('Employee Added', success)
+
+                UI.clearFields();
+        }
+
+
+});
+
+document.querySelector('#output').addEventListener('click', (e) => {
+        UI.deleteEmployee(e.target);
+
+        Store.removeEmployee(e.target.parentElement);
+})
